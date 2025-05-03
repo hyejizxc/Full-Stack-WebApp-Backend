b6238f60d14429f671ac1c0ae69b2a831f56d410
Jim <jimfromyt@gmail.com>
2025-05-04 00:59:50 +0800

    Initial commit

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: 'https://full-stack-web-app-frontend.vercel.app',
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // MongoDB Connection
